05b74d439b36138376c5608ef5089ee4dc31cabe
ameanasad <amean.asad1999@gmail.com>
2025-05-02 09:18:00 -0700

    feat: add tdx attestation report generation

---
+++ src/attestation.rs
@@ -0,0 +1,32 @@
+use base64;
+use dcap_rs::types::quotes::version_4::QuoteV4;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use std::io::Write;
+use tdx::Tdx;
+
+pub fn get_raw_attestation_report() -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let tdx = Tdx::new();
+    let raw_report = tdx.get_attestation_report_raw()?;
+    Ok(raw_report)
+}
+
+pub fn get_parsed_attestation_report() -> Result<QuoteV4, Box<dyn std::error::Error>> {
+    let raw_report = get_raw_attestation_report()?;
+    let report = QuoteV4::from_bytes(&raw_report);
+    Ok(report)
+}
+
+pub fn get_compressed_encoded_attestation() -> Result<String, Box<dyn std::error::Error>> {
+    let raw_report = get_raw_attestation_report()?;
+    let compressed_report = compress_gzip(&raw_report)?;
+    let encoded_report = base64::encode(&compressed_report);
+    Ok(encoded_report)
+}
+
+fn compress_gzip(data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(data)?;
+    let compressed = encoder.finish()?;
+    Ok(compressed)
+}
