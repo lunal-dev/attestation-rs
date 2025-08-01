156d1a95bc82b0a5ea9e70b13233d893c80d562e
ameanasad <amean.asad1999@gmail.com>
2025-07-31 20:23:01 -0700

    add coco-provider to deps

--- lunal-attestation/src/bin/attest_sev_snp.rs
+++ lunal-attestation/src/bin/attest_sev_snp.rs
@@ -3,36 +3,35 @@ fn main() {
     {
         use clap::{Arg, Command};
         use lunal_attestation::sev_snp::attest::*;
+        let data: &str = "hello";
 
-        let matches = Command::new("attest")
-            .version(env!("CARGO_PKG_VERSION"))
-            .about("Attestation report tool")
-            .arg(
-                Arg::new("format")
-                    .short('f')
-                    .long("format")
-                    .value_parser(["raw", "compressed"])
-                    .default_value("json")
-                    .help("Output format"),
-            )
-            .arg(
-                Arg::new("pretty")
-                    .long("pretty")
-                    .action(clap::ArgAction::SetTrue)
-                    .help("Pretty print JSON"),
-            )
-            .get_matches();
+        get_attestation_with_data(data).expect("Failed to get raw attestation");
+        // let matches = Command::new("attest")
+        //     .version(env!("CARGO_PKG_VERSION"))
+        //     .about("Attestation report tool")
+        //     .arg(
+        //         Arg::new("format")
+        //             .short('f')
+        //             .long("format")
+        //             .value_parser(["raw", "compressed"])
+        //             .default_value("json")
+        //             .help("Output format"),
+        //     )
+        //     .arg(
+        //         Arg::new("pretty")
+        //             .long("pretty")
+        //             .action(clap::ArgAction::SetTrue)
+        //             .help("Pretty print JSON"),
+        //     )
+        //     .get_matches();
 
-        let result = match matches.get_one::<String>("format").unwrap().as_str() {
-            "raw" => {
-                let data: &str = "hello";
-                get_attestation_with_data(data).expect("Failed to get raw attestation");
-                Ok("test");
-            }
-            _ => unreachable!(),
-        };
-
-        println!("{}", result);
+        // let result = match matches.get_one::<String>("format").unwrap().as_str() {
+        //     "raw" => {
+        //         let data: &str = "hello";
+        //         get_attestation_with_data(data).expect("Failed to get raw attestation");
+        //     }
+        //     _ => unreachable!(),
+        // };
     }
 
     #[cfg(not(feature = "attestation"))]
