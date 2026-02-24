58896fda06f466e33f523b63f1eadc9d684559cc
ameanasad <amean.asad1999@gmail.com>
2026-02-23 17:30:33 -0800

    feat: add platform features again

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -1,5 +1,10 @@
-pub mod tdx;
+#[cfg(feature = "snp")]
 pub mod snp;
+#[cfg(feature = "tdx")]
+pub mod tdx;
+#[cfg(any(feature = "az-snp", feature = "az-tdx"))]
 pub mod tpm_common;
-pub mod az_tdx;
+#[cfg(feature = "az-snp")]
 pub mod az_snp;
+#[cfg(feature = "az-tdx")]
+pub mod az_tdx;
